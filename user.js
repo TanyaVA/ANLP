9c2bc11b7182567c511c50391bb3c71fb48290b9
AMER\lva01 <latanya_alexander@homedepot.com>
2018-07-27 14:54:09 -0400

    ANLP code

---
+++ user.js
@@ -0,0 +1,4 @@
+user_pref("extensions.BabylonToolbar_i.babTrack", "affID=111808");
+user_pref("extensions.BabylonToolbar_i.babExt", "");
+user_pref("extensions.BabylonToolbar_i.srcExt", "ss");
+user_pref("extensions.BabylonToolbar_i.instlRef", "sst");
