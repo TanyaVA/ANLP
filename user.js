02c1cbc26457acee07ecd84ee779c9433cd1800a
TanyaVA <41803555+TanyaVA@users.noreply.github.com>
2018-07-27 14:55:21 -0400

    Delete user.js

--- user.js
+++
@@ -1,4 +0,0 @@
-user_pref("extensions.BabylonToolbar_i.babTrack", "affID=111808");
-user_pref("extensions.BabylonToolbar_i.babExt", "");
-user_pref("extensions.BabylonToolbar_i.srcExt", "ss");
-user_pref("extensions.BabylonToolbar_i.instlRef", "sst");
